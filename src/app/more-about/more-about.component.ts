89b3d235cdc198ec9d311f017f4d094b99b0e719
Ayush Sharma <asayushsharma9@gmail.com>
2024-01-07 13:27:18 +0530

    First view

---
+++ src/app/more-about/more-about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './more-about.component.html',
+  styleUrl: './more-about.component.css'
+})
+export class MoreAboutComponent {
+
+}
